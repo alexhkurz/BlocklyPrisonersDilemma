5efa7a69c5a418e9e899ad05c420628a908718e7
Alexander Kurz <alexhkurz@gmail.com>
2023-10-29 22:02:19 -0700

    aider: Move `getLastMove` function inside `env` object and update references to it in `runGame` function.
    # Aider chat conversation:
    
    ASSISTANT: Yes, we can move the `getLastMove` function inside the `env` object in the `game_logic.js` file. This will make it accessible as `env.getLastMove` in the `runGame` function.
    
    Here are the changes:
    
    ```javascript
    server/game_logic.js
    <<<<<<< SEARCH
        // A function to get the last move of a player
        getLastMove: function(player) {
            for (let i = this.env.moves.length - 1; i >= 0; i--) {
                if (this.env.moves[i].player === player) {
                    return this.env.moves[i].move;
                }
            }
            return null;
        },
    =======
        env: {
            // The global state of the game
            players: [], // An array to store the players
            moves: [], // An array to store the moves of the players
            gameMatrix: { // The game matrix
                'cooperate': {
                    'cooperate': [2, 2],
                    'defect': [0, 3]
                },
                'defect': {
                    'cooperate': [3, 0],
                    'defect': [1, 1]
                }
            },
            numberOfRounds: 5, // The number of rounds in the game
            payoff: [0, 0], // The payoff for each player
    
            // A function to get the last move of a player
            getLastMove: function(player) {
                for (let i = this.moves.length - 1; i >= 0; i--) {
                    if (this.moves[i].player === player) {
                        return this.moves[i].move;
                    }
                }
                return null;
            },
        },
    >>>>>>> REPLACE
    ```
    
    ```javascript
    server/game_logic.js
    <<<<<<< SEARCH
                // Execute the code
                eval('let getLastMove = ' + this.getLastMove.toString() + '; let opponent = 1; ' + code0 );
                let move0 = move;
                eval('let getLastMove = ' + this.getLastMove.toString() + '; let opponent = 0; ' + code1 );
                let move1 = move;
    =======
                // Execute the code
                eval('let getLastMove = ' + this.env.getLastMove.toString() + '; let opponent = 1; ' + code0 );
                let move0 = move;
                eval('let getLastMove = ' + this.env.getLastMove.toString() + '; let opponent = 0; ' + code1 );
                let move1 = move;
    >>>>>>> REPLACE
    ```
    
    Please apply these changes and let me know if you encounter any issues.

--- server/game_logic.js
+++ server/game_logic.js
@@ -27,14 +27,32 @@ module.exports = {
         this.env.moves.push({player: player, move: move});
     },
 
-    // A function to get the last move of a player
-    getLastMove: function(player) {
-        for (let i = this.env.moves.length - 1; i >= 0; i--) {
-            if (this.env.moves[i].player === player) {
-                return this.env.moves[i].move;
+    env: {
+        // The global state of the game
+        players: [], // An array to store the players
+        moves: [], // An array to store the moves of the players
+        gameMatrix: { // The game matrix
+            'cooperate': {
+                'cooperate': [2, 2], 
+                'defect': [0, 3] 
+            },
+            'defect': {
+                'cooperate': [3, 0], 
+                'defect': [1, 1] 
             }
-        }
-        return null;
+        },
+        numberOfRounds: 5, // The number of rounds in the game
+        payoff: [0, 0], // The payoff for each player
+
+        // A function to get the last move of a player
+        getLastMove: function(player) {
+            for (let i = this.moves.length - 1; i >= 0; i--) {
+                if (this.moves[i].player === player) {
+                    return this.moves[i].move;
+                }
+            }
+            return null;
+        },
     },
 
     // A function to reset the game
@@ -58,9 +76,9 @@ module.exports = {
             let move;
 
             // Execute the code
-            eval('let getLastMove = ' + this.getLastMove.toString() + '; let opponent = 1; ' + code0 );
+            eval('let getLastMove = ' + this.env.getLastMove.toString() + '; let opponent = 1; ' + code0 );
             let move0 = move;
-            eval('let getLastMove = ' + this.getLastMove.toString() + '; let opponent = 0; ' + code1 );
+            eval('let getLastMove = ' + this.env.getLastMove.toString() + '; let opponent = 0; ' + code1 );
             let move1 = move;
 
             // Record the moves
