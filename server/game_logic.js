86165fcbeb099d4eeab281263542e74a642b069c
Alexander Kurz <alexhkurz@gmail.com>
2023-10-29 17:52:49 -0700

    aider: Created game_logic.js file in the server directory with basic game logic and global state tracking.
    # Aider chat conversation:
    
    USER: ok, let us do some server side stuff now; we need to think about the game logic; I want to have a object "env" that keeps track of the global state of the game; at the moment I wonly want to players; there will be two files, server.js and game_logic.js; let us do some game_logic.js first; what do you propose?
    
    ASSISTANT: Sure, we can start by creating a basic structure for the `game_logic.js` file. This file will contain the game logic and the `env` object that will keep track of the global state of the game.
    
    Here's a basic structure for the `game_logic.js` file:
    
    ```javascript
    module.exports = {
        env: {
            // The global state of the game
            players: [], // An array to store the players
            moves: [], // An array to store the moves of the players
        },
    
        // A function to add a player to the game
        addPlayer: function(player) {
            this.env.players.push(player);
        },
    
        // A function to record a move
        recordMove: function(player, move) {
            this.env.moves.push({player: player, move: move});
        },
    
        // A function to get the last move of a player
        getLastMove: function(player) {
            for (let i = this.env.moves.length - 1; i >= 0; i--) {
                if (this.env.moves[i].player === player) {
                    return this.env.moves[i].move;
                }
            }
            return null;
        },
    
        // A function to reset the game
        reset: function() {
            this.env.players = [];
            this.env.moves = [];
        }
    };
    ```
    
    This is a very basic structure and can be expanded upon based on the specific requirements of your game. For example, you might want to add functions to calculate the score of a player, to determine the winner of the game, etc.
    
    USER: can you comit this in the folder server
    
    ASSISTANT: Sure, I can provide the necessary *SEARCH/REPLACE block* for creating the `game_logic.js` file in the `server` directory.
    
    ```javascript
    server/game_logic.js
    <<<<<<< SEARCH
    
    =======
    module.exports = {
        env: {
            // The global state of the game
            players: [], // An array to store the players
            moves: [], // An array to store the moves of the players
        },
    
        // A function to add a player to the game
        addPlayer: function(player) {
            this.env.players.push(player);
        },
    
        // A function to record a move
        recordMove: function(player, move) {
            this.env.moves.push({player: player, move: move});
        },
    
        // A function to get the last move of a player
        getLastMove: function(player) {
            for (let i = this.env.moves.length - 1; i >= 0; i--) {
                if (this.env.moves[i].player === player) {
                    return this.env.moves[i].move;
                }
            }
            return null;
        },
    
        // A function to reset the game
        reset: function() {
            this.env.players = [];
            this.env.moves = [];
        }
    };
    >>>>>>> REPLACE
    ```
    
    Please create a new file `game_logic.js` in the `server` directory and paste the code from the `REPLACE` section into it.

---
+++ server/game_logic.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+        // The global state of the game
+        players: [], // An array to store the players
+        moves: [], // An array to store the moves of the players
+    },
+
+    // A function to add a player to the game
+    addPlayer: function(player) {
+        this.env.players.push(player);
+    },
+
+    // A function to record a move
+    recordMove: function(player, move) {
+        this.env.moves.push({player: player, move: move});
+    },
+
+    // A function to get the last move of a player
+    getLastMove: function(player) {
+        for (let i = this.env.moves.length - 1; i >= 0; i--) {
+            if (this.env.moves[i].player === player) {
+                return this.env.moves[i].move;
+            }
+        }
+        return null;
+    },
+
+    // A function to reset the game
+    reset: function() {
+        this.env.players = [];
+        this.env.moves = [];
+    }
+};
